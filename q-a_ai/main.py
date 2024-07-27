1640f13da3b5de95673a39d0c06dc3f8d0992b4f
RohanSaxena2020 <71786640+RohanSaxena2020@users.noreply.github.com>
2024-07-26 20:42:13 -0400

    Update main.py

--- q-a_ai/main.py
+++ q-a_ai/main.py
@@ -1,44 +1,16 @@
-from dotenv import load_dotenv
-import os
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
-import langchain.llms as llms
-from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain_community.vectorstores import FAISS
-from langchain_community.llms import GooglePalm
+import streamlit as st
+from langchain_helper import get_qa_chain, create_vector_db
 
-# Load the .env file
-load_dotenv()
-
-# Get the API key
-api_key = os.getenv('GOOGLE_API_KEY')
-
-# Use the internal function to import GooglePalm
-GooglePalm = llms._import_google_palm()
-
-# Initialize the GooglePalm instance with the API key
-llm = GooglePalm(google_api_key=api_key, temperature=0.7)
-
-# Initialize instructor embeddings using the Hugging Face model
-instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
-e = instructor_embeddings.embed_query("What is your refund policy?")
-
-vectordb_file_path = 'faiss_index'
-
-def create_vector_db():
-    # Load data from FAQ sheet
-    file_path = './codebasics_faqs.csv'
-    loader = CSVLoader(file_path=file_path, source_column="prompt", encoding="latin1")
-    data = loader.load()
-
-    # Create a FAISS instance for vector database from 'data'
-    vectordb = FAISS.from_documents(documents=data,
-                                    embedding=instructor_embeddings)
-
-    # Save vector database locally
-    vectordb.save_local(vectordb_file_path)
-
-if __name__ == '__main__':
+st.title("Codebasics Q&A 🌱")
+btn = st.button("Create Knowledgebase")
+if btn:
     create_vector_db()
 
-# # Create a retriever for querying the vector database
-# retriever = vectordb.as_retriever(score_threshold = 0.7)
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_qa_chain()
+    response = chain({"query": question})
+
+    st.header("Answer")
+    st.write(response["result"])
